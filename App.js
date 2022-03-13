0a15bbc9a26b42f7342b2b573f32e53254526e69
Mai Huy <trinhmaihuyak53@gmail.com>
2022-03-13 13:37:18 +0700

    #9

--- App.js
+++ App.js
@@ -1,37 +1,40 @@
 import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View, Linking} from 'react-native';
+import {Button, StyleSheet, Text, View, Linking, ScrollView, RefreshControl} from 'react-native';
 
 const App = () => {
+  const [items, setItem] = useState([
+    {key: 1, content: 1},
+    {key: 2, content: 2},
+    {key: 3, content: 3},
+    {key: 4, content: 4},
+    {key: 5, content: 5},
+    {key: 6, content: 6},
+    {key: 7, content: 7},
+    {key: 8, content: 8},
+    {key: 9, content: 9},
+    {key: 10, content: 10},
+  ]);
+
+  const [refresh, setRefresh] = useState(false);
+
+  const onFresh = () => {
+    setRefresh(true);
+    setItem([...items, {key: 11, content: 11}]);
+    setRefresh(false);
+  };
+
   return (
-    <View style={styles.body}>
-      <View style={styles.row1}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.row2}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-      <View style={styles.row3}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
-    </View>
+    <ScrollView
+      style={styles.body}
+      refreshControl={<RefreshControl refreshing={refresh} onRefresh={onFresh}/>}>
+      {items.map(object => {
+        return (
+          <View style={styles.item} key={object.key}>
+            <Text style={styles.text}>{object.content}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 };
 
@@ -39,62 +42,14 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
   },
-  row1: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  view1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red',
-  },
-  view2: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'green',
-  },
-  view3: {
-    flex: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'blue',
-  },
-  row2: {
-    flex: 2,
-    flexDirection: 'column',
-  },
-  view4: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  item: {
     backgroundColor: 'orange',
-  },
-  view5: {
-    flex: 1,
-    justifyContent: 'center',
+    margin: 10,
     alignItems: 'center',
-    backgroundColor: 'yellow',
-  },
-  row3: {
-    flex: 7,
-    flexDirection: 'row',
-  },
-  view6: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-  view7: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'pink',
+    justifyContent: 'center'
   },
   text: {
-    fontSize: 24,
+    fontSize: 45,
     color: 'black',
     fontWeight: '700',
   },
